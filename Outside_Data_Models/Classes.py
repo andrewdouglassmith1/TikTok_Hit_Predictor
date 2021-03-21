b68cc7c0a9b9b6dfb538d4956d2add98e4260336
Andrew-Smith <andrewdouglassmith1@gmail.com>
2021-03-21 17:23:49 -0400

    new classes

--- Outside_Data_Models/Classes.py
+++ Outside_Data_Models/Classes.py
@@ -3,6 +3,8 @@ import pandas as pd
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score, recall_score, precision_recall_curve,f1_score, fbeta_score, accuracy_score
 
 #Functions to create daily urls
 
@@ -68,3 +70,23 @@ def make_confusion_matrix(model,X_val,y_val, threshold=0.5):
            yticklabels=['not a success', 'success']);
     plt.xlabel('prediction')
     plt.ylabel('actual')
+
+# Function to return train / test results
+def train_scores(model, x_train, y_train):
+    """
+    Returns training accuracy score
+    """
+    print("Train Scores")
+    print("Accuracy score: {:6.2f}%".format(100*model.score(x_train, y_train)))
+
+def test_scores(y_test, y_predict):
+    """
+    Returns various test scores
+    """
+    print("Test Scores")
+    print("Score: {:6.2f}%".format(100*accuracy_score(y_test,y_predict)))
+    print("F1 score: {:6.2f}%".format(100*f1_score(y_test,y_predict)))
+    print("Precision: {:6.2f}%,  Test Recall: {:6.2f}%".format(100*precision_score(y_test,y_predict),
+                                                     100*recall_score(y_test,y_predict)))
+
+# Create a get dummies function based on cuttoff point of ranking for top artists
